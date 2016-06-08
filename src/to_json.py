f08789e3926a56034b491b7b6362fc915fc4e386
Beshr Kayali <beshrkayali@gmail.com>
2016-06-09 01:13:36 +0200

    Simple csv to json converter with aggregation

---
+++ src/to_json.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+from csv import DictReader
+import json
+import io
+
+
+class CsvToJson(object):
+    def __init__(self, csv_file, json_output, aggregate_key):
+        self.csv_file = csv_file
+        self.json_output = json_output
+        self.aggregate_key = aggregate_key
+
+    def read_csv(self):
+        output = defaultdict(list)
+        with open(self.csv_file, 'r') as csvfile:
+            reader = DictReader(csvfile)
+
+            for row in reader:
+                c = row.pop(self.aggregate_key)
+                output[c].append(row)
+
+        return output
+
+    def to_json(self):
+        data = self.read_csv()
+
+        with io.open(self.json_output, 'w', encoding='utf-8') as jsonfile:
+            for row in data.items():
+                jsonfile.write(
+                    json.dumps(row, ensure_ascii=False).decode("utf-8")
+                )
+                jsonfile.write(u'\n')
+
+if __name__ == '__main__':
+    ctj = CsvToJson(
+        csv_file='../postcodes.csv',
+        json_output='../postcodess.json',
+        aggregate_key='Code'
+    )
+
+    ctj.to_json()
